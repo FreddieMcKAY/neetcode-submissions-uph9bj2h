2d5647b77647440e8717cf035d7cb46082123450
Freddie McKay <145266888+FreddieMcKAY@users.noreply.github.com>
2026-04-26 12:31:39 +0100

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+
+        if (nums.Length == 0) {
+            return 0;
+        }
+
+        HashSet<int> numbers = new HashSet<int>();
+        List<int> heads = new List<int>();
+        int max = 1;
+
+        foreach (int num in nums) {
+            if (!numbers.Contains(num)) {
+                numbers.Add(num);
+            }
+        }
+
+        foreach (int num in numbers) {
+            if (!numbers.Contains(num - 1)) {
+                heads.Add(num);
+            }
+        }
+
+        foreach (int num in heads) {
+            int count = 1;
+            bool ended = false;
+            int next = num + 1;
+
+            while (!ended) {
+                if (numbers.Contains(next)) {
+                    count++;
+                    next++;
+                } else {
+                    ended = true;
+                }
+            }
+
+            max = Math.Max(max, count);
+        }
+        return max;
+    }
+}
