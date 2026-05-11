570d2c9e1448a18c09fed2088b9206e4aecf6252
Freddie McKay <145266888+FreddieMcKAY@users.noreply.github.com>
2026-05-11 12:43:43 +0100

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.cs
@@ -0,0 +1,28 @@
+public class MyHashSet {
+
+    HashSet<int> set;
+
+    public MyHashSet() {
+        this.set = new HashSet<int>();
+    }
+    
+    public void Add(int key) {
+        set.Add(key);
+    }
+    
+    public void Remove(int key) {
+        set.Remove(key);
+    }
+    
+    public bool Contains(int key) {
+        return set.Contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * bool param_3 = obj.Contains(key);
+ */
