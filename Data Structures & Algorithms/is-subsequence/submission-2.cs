091e0052e5d4aa63269b5b16b0078883204fb9d6
Freddie McKay <145266888+FreddieMcKAY@users.noreply.github.com>
2026-05-14 22:49:30 +0100

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+
+        if (s == null || s.Length == 0) {
+            return true;
+        }
+
+        int sPointer = 0;
+        int tPointer = 0;
+
+        while (sPointer < s.Length && tPointer < t.Length) {
+            
+            if (s[sPointer] == t[tPointer]) {
+                sPointer++;
+            }
+            tPointer++;
+        }
+
+        return sPointer == s.Length;
+        
+    }
+}
